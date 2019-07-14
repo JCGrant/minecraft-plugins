dd27e80fcb270650cc8101eae962fffc7467ae95
JCGrant <jamescolin.grant@gmail.com>
2019-07-14 03:38:51 +0100

    Allow users to set their chat mode to local/global

--- src/main/java/com/jcgrant/jccraft/ChatLimiter.java
+++ src/main/java/com/jcgrant/jccraft/ChatLimiter.java
@@ -1,22 +1,80 @@
 package com.jcgrant.jccraft;
 
+import java.util.HashMap;
+
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class ChatLimiter implements Listener {
 
   private final int MAX_CHAT_DISTANCE = 100;
+  private final String CONFIG_KEY = "chat-modes";
+  private final String GLOBAL = "global";
+  private final String LOCAL = "local";
+
+  private JavaPlugin plugin;
+
+  public ChatLimiter(JavaPlugin plugin) {
+    this.plugin = plugin;
+    plugin.getConfig().addDefault(CONFIG_KEY, new HashMap<String, String>());
+  }
 
   @EventHandler
   public void onPlayerChat(AsyncPlayerChatEvent e) {
+    e.setCancelled(true);
     Player sender = e.getPlayer();
-    e.getRecipients().removeIf((r) -> !canHearMessage(r, sender));
+    e.getRecipients().stream()
+    .forEach(r -> {
+      ChatColor color = ChatColor.WHITE;
+      if (!isGlobal(sender)) {
+        color = ChatColor.GREEN;
+      }
+      if (canHearMessage(r, sender)) {
+        r.sendMessage(color + "<" + sender.getName() + "> " + e.getMessage());
+      }
+    });
+  }
+
+  public boolean setChatMode(Player player, String[] args) {
+    if (args.length != 1) {
+      player.sendMessage(ChatColor.RED + "usage: /chat <global/local>");
+      return true;
+    }
+    String arg = args[0].toLowerCase();
+    if (arg.equals("global")) {
+      setChatMode(player, GLOBAL);
+      player.sendMessage(ChatColor.AQUA + "Set chat mode to Global");
+      return true;
+    } else if (arg.equals("local")) {
+      setChatMode(player, LOCAL);
+      player.sendMessage(ChatColor.AQUA + "Set chat mode to Local");
+      return true;
+    } else {
+      player.sendMessage(ChatColor.RED + "usage: /chat <global/local>");
+      return true;
+    }
   }
 
   private boolean canHearMessage(Player receiver, Player sender)  {
-    return sender.getWorld() == receiver.getWorld() &&
+    return isGlobal(sender) ||
+        sender.getWorld() == receiver.getWorld() &&
         sender.getLocation().distance(receiver.getLocation()) <= MAX_CHAT_DISTANCE;
+  }
+
+  private boolean isGlobal(Player sender) {
+    return getChatMode(sender) == GLOBAL;
+  }
+
+  private String getChatMode(Player p) {
+    return plugin.getConfig().getString(CONFIG_KEY + "." + p.getName(), GLOBAL);
+  }
+
+  private void setChatMode(Player p, String mode) {
+    plugin.getConfig().set(CONFIG_KEY + "." + p.getName(), mode);
+    plugin.saveConfig();
   }
 }
--- src/main/java/com/jcgrant/jccraft/JCCraft.java
+++ src/main/java/com/jcgrant/jccraft/JCCraft.java
@@ -1,16 +1,39 @@
 package com.jcgrant.jccraft;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class JCCraft extends JavaPlugin {
+
+  private ChatLimiter chatLimiter;
+
   @Override
   public void onEnable() {
     getServer().getLogger().info("JCCraft Suite is enabled.");
-    getServer().getPluginManager().registerEvents(new ChatLimiter(), this);
+    chatLimiter = new ChatLimiter(this);
+    getServer().getPluginManager().registerEvents(chatLimiter, this);
+    getConfig().options().copyDefaults(true);
+    saveConfig();
   }
 
   @Override
   public void onDisable() {
     getServer().getLogger().info("JCCraft Suite is disabled.");
+    saveConfig();
+  }
+
+  @Override
+  public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    if (!(sender instanceof Player)) {
+      return false;
+    }
+    String commandName = command.getName().toLowerCase();
+    switch (commandName) {
+    case "chat":
+      return chatLimiter.setChatMode((Player) sender, args);
+    }
+    return false;
   }
 }
